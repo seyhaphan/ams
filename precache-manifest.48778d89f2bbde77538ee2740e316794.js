68a50f94957e33084709d4fcb6401dff79841a25
seyha phan <phan.seiha2017@gmail.com>
2020-07-11 00:17:26 +0700

    Updates

---
+++ precache-manifest.48778d89f2bbde77538ee2740e316794.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e38d06a9ad8e4c7d4632074a35276a",
+    "url": "/ams/index.html"
+  },
+  {
+    "revision": "74f8447ca0305856d0cd",
+    "url": "/ams/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "18f71bf1cffaaedfc647",
+    "url": "/ams/static/css/main.f1e01815.chunk.css"
+  },
+  {
+    "revision": "74f8447ca0305856d0cd",
+    "url": "/ams/static/js/2.0d22f493.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ams/static/js/2.0d22f493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18f71bf1cffaaedfc647",
+    "url": "/ams/static/js/main.48387d33.chunk.js"
+  },
+  {
+    "revision": "7902085a8e62b42a5751",
+    "url": "/ams/static/js/runtime-main.b8ccb6f8.js"
+  },
+  {
+    "revision": "f368c4310c12d5c8cd49a9de29153691",
+    "url": "/ams/static/media/logo.f368c431.png"
+  }
+]);
